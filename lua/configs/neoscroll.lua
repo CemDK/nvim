e1b31f3d66f4b4160be790cba6c6867b11ec7318
CemDK <25245902+CemDK@users.noreply.github.com>
2025-03-13 22:58:16 +0100

    update

--- lua/configs/neoscroll.lua
+++ lua/configs/neoscroll.lua
@@ -3,15 +3,16 @@ neoscroll.setup {
     -- post_hook = function(info)
     --     vim.api.nvim_feedkeys("zz", "n", false)
     -- end,
-    easing = "sine", -- "quadratic", "sine", "linear", "circular"
+    easing = "linear", -- "quadratic", "sine", "linear", "circular"
+    duration_multiplier = 0.1,
 }
 
 local keymap = {
     ["<C-u>"] = function()
-        neoscroll.ctrl_u { duration = 200 }
+        neoscroll.ctrl_u { duration = 10 }
     end,
     ["<C-d>"] = function()
-        neoscroll.ctrl_d { duration = 200 }
+        neoscroll.ctrl_d { duration = 10 }
     end,
     ["<C-b>"] = function()
         neoscroll.ctrl_b { duration = 200 }
