d26cd2a23937af6ef623719c9e120add938f3acf
Cem Deniz Kabakci <CemDenizKabakci@outlook.com>
2025-03-29 14:40:52 +0100

    update

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -1,6 +1,7 @@
 dofile(vim.g.base46_cache .. "telescope")
+local telescope = require "telescope"
 
-return {
+telescope.setup {
     defaults = {
         prompt_prefix = "   ",
         selection_caret = " ",
@@ -20,5 +21,15 @@ return {
     },
 
     extensions_list = { "themes", "terms" },
-    extensions = {},
+    extensions = {
+        fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- "ignore_case" or "respect_case" or "smart_case"
+        },
+    },
 }
+
+telescope.load_extension "fzf"
+telescope.load_extension "lazygit"
