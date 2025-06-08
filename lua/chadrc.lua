fc2b2afc8be53122ae2f6560fdc0ead6f8703238
CemDK <25245902+CemDK@users.noreply.github.com>
2025-06-08 16:18:06 +0200

    update
    - change floating terminal border

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -178,7 +178,7 @@ M.term = {
         col = 0.2,
         width = 0.6,
         height = 0.8,
-        border = "double", -- "single", "double", "rounded", "solid", "shadow", "none"
+        border = "none", -- "single", "double", "rounded", "solid", "shadow", "none"
     },
 }
 
