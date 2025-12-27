f04eda0b74a2ad58f8a415c707d05565bce818e8
CemDK <25245902+CemDK@users.noreply.github.com>
2025-12-27 19:06:28 +0100

    update
    - make linter happy

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,68 +1,73 @@
+---@class vim.var_accessor
+local global = vim.g
+
+
 -- Neovide settings
-vim.g.neovide_refresh_rate = 144
-vim.g.neovide_refresh_rate_idle = 5
-vim.g.neovide_fullscreen = false
-vim.g.neovide_profiler = false
+global.neovide_refresh_rate = 144
+global.neovide_refresh_rate_idle = 5
+global.neovide_fullscreen = false
+global.neovide_profiler = false
 vim.o.guifont = vim.fn.hostname() == "Cem-Ryzen" and "RobotoMono Nerd Font:h10" or "MesloLGS Nerd Font:h12"
 
 -- Text rendering
 vim.opt.linespace = -2
-vim.g.neovide_text_gamma = 0.0
-vim.g.neovide_text_contrast = 0.5 -- Ayo I dunno what values are good here, I don't see any difference
+global.neovide_text_gamma = 0.0
+global.neovide_text_contrast = 0.5 -- Ayo I dunno what values are good here, I don't see any difference
 
 -- Cursor
-vim.g.neovide_cursor_antialiasing = true
-vim.g.neovide_cursor_animation_length = 0.100
-vim.g.neovide_cursor_short_animation_length = 0.04
-vim.g.neovide_cursor_trail_size = 1.0
-vim.g.neovide_cursor_animate_in_insert_mode = true
-vim.g.neovide_cursor_animate_command_line = true
--- vim.g.neovide_cursor_vfx_mode = "railgun"
--- vim.g.neovide_cursor_trail_size = 0.5
+global.neovide_cursor_antialiasing = true
+global.neovide_cursor_animation_length = 0.100
+global.neovide_cursor_short_animation_length = 0.04
+global.neovide_cursor_trail_size = 1.0
+global.neovide_cursor_animate_in_insert_mode = true
+global.neovide_cursor_animate_command_line = true
+-- global.neovide_cursor_vfx_mode = "railgun"
+-- global.neovide_cursor_trail_size = 0.5
 
 -- Padding
 
 if vim.fn.hostname() == "Cem-Ryzen" then
-    vim.g.neovide_padding_bottom = 1
-    vim.g.neovide_padding_top = 1
-    vim.g.neovide_padding_right = 0
-    vim.g.neovide_padding_left = 1
+    global.neovide_padding_bottom = 1
+    global.neovide_padding_top = 1
+    global.neovide_padding_right = 0
+    global.neovide_padding_left = 1
 else
-    vim.g.neovide_padding_bottom = 16
-    vim.g.neovide_padding_top = 16
-    vim.g.neovide_padding_right = 16
-    vim.g.neovide_padding_left = 16
+    global.neovide_padding_bottom = 16
+    global.neovide_padding_top = 16
+    global.neovide_padding_right = 16
+    global.neovide_padding_left = 16
 end
 
 -- Floating windows shadow
-vim.g.neovide_floating_shadow = true
-vim.g.neovide_floating_z_height = 5
-vim.g.neovide_light_angle_degrees = 75
-vim.g.neovide_light_radius = 5
-vim.g.neovide_floating_corner_radius = 0.0
+global.neovide_floating_shadow = true
+global.neovide_floating_z_height = 5
+global.neovide_light_angle_degrees = 75
+global.neovide_light_radius = 5
+global.neovide_floating_corner_radius = 0.0
 
 -- Opacity
-vim.g.neovide_opacity = vim.fn.hostname() == "Cem-Ryzen" and 1.0 or 1 --0.90
-vim.g.neovide_normal_opacity = 1
-vim.g.neovide_window_blurred = false -- for macOS only atm
-vim.g.neovide_floating_blur_amount_x = 2.0
-vim.g.neovide_floating_blur_amount_y = 2.0
+global.neovide_opacity = vim.fn.hostname() == "Cem-Ryzen" and 1.0 or 1 --0.90
+global.neovide_normal_opacity = 1
+global.neovide_window_blurred = false                                  -- for macOS only atm
+global.neovide_floating_blur_amount_x = 2.0
+global.neovide_floating_blur_amount_y = 2.0
 --
-vim.g.neovide_position_animation_length = 0.15
+global.neovide_position_animation_length = 0.15
 
 -- Scrolling
-vim.g.neovide_scroll_animation_length = 0.3
-vim.g.neovide_scroll_animation_far_lines = 1
+global.neovide_scroll_animation_length = 0.3
+global.neovide_scroll_animation_far_lines = 1
 
 -- Misc
---vim.g.neovide_cursor_hack = true
-vim.g.neovide_hide_mouse_when_typing = true
+--global.neovide_cursor_hack = true
+global.neovide_hide_mouse_when_typing = true
 
 -- Keybindings
-vim.g.neovide_input_macos_option_key_is_meta = "only_left" -- Interprets Alt + whatever actually as <M-whatever>, instead of sending the actual special character to Neovim.
-vim.keymap.set("n", "<C-v>", '"+P') -- Paste normal mode
-vim.keymap.set("v", "<C-v>", '"+P') -- Paste visual mode
-vim.keymap.set("c", "<C-v>", "<C-R>+") -- Paste command mode
+global.neovide_input_macos_option_key_is_meta =
+"only_left"                                 -- Interprets Alt + whatever actually as <M-whatever>, instead of sending the actual special character to Neovim.
+vim.keymap.set("n", "<C-v>", '"+P')         -- Paste normal mode
+vim.keymap.set("v", "<C-v>", '"+P')         -- Paste visual mode
+vim.keymap.set("c", "<C-v>", "<C-R>+")      -- Paste command mode
 vim.keymap.set("i", "<C-v>", '<ESC>l"+Pli') -- Paste insert mode
 
 -- Allow clipboard copy paste in neovim
@@ -72,23 +77,23 @@ vim.api.nvim_set_keymap("t", "<C-v>", "<C-R>+", { noremap = true, silent = true 
 vim.api.nvim_set_keymap("v", "<C-v>", "<C-R>+", { noremap = true, silent = true })
 
 -- Neovide terminal colors
-vim.g.terminal_color_0 = "#005F60"
-vim.g.terminal_color_1 = "#ff4500"
-vim.g.terminal_color_2 = "#8ec07c"
-vim.g.terminal_color_3 = "#f78104"
-vim.g.terminal_color_4 = "#268bd2"
-vim.g.terminal_color_5 = "#a277ff"
-vim.g.terminal_color_6 = "#2aa198"
-vim.g.terminal_color_7 = "#a6b2b2"
-vim.g.terminal_color_8 = "#003c3d"
-vim.g.terminal_color_9 = "#ff7f50"
-vim.g.terminal_color_10 = "#a9d196"
-vim.g.terminal_color_11 = "#ffa940"
-vim.g.terminal_color_12 = "#5eafed"
-vim.g.terminal_color_13 = "#c4a6ff"
-vim.g.terminal_color_14 = "#4ec2b8"
-vim.g.terminal_color_15 = "#7a8787"
-vim.g.terminal_ansi_colors = {
+global.terminal_color_0 = "#005F60"
+global.terminal_color_1 = "#ff4500"
+global.terminal_color_2 = "#8ec07c"
+global.terminal_color_3 = "#f78104"
+global.terminal_color_4 = "#268bd2"
+global.terminal_color_5 = "#a277ff"
+global.terminal_color_6 = "#2aa198"
+global.terminal_color_7 = "#a6b2b2"
+global.terminal_color_8 = "#003c3d"
+global.terminal_color_9 = "#ff7f50"
+global.terminal_color_10 = "#a9d196"
+global.terminal_color_11 = "#ffa940"
+global.terminal_color_12 = "#5eafed"
+global.terminal_color_13 = "#c4a6ff"
+global.terminal_color_14 = "#4ec2b8"
+global.terminal_color_15 = "#7a8787"
+global.terminal_ansi_colors = {
     "#005F60",
     "#ff4500",
     "#8ec07c",
@@ -107,4 +112,4 @@ vim.g.terminal_ansi_colors = {
     "#7a8787",
 }
 
-vim.g.highlight_Terminal = "guibg=#005F60 guifg=#93a1a1"
+global.highlight_Terminal = "guibg=#005F60 guifg=#93a1a1"
