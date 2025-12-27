cb2d54d374fab3f0b65bd7020589c3118dd4a11e
Cem Deniz Kabakci <CemDenizKabakci@outlook.com>
2025-12-27 19:06:28 +0100

    update
    - make linter happy

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -14,16 +14,19 @@ return {
                     "/usr/share/lua/5.3/?/init.lua",
                 },
             },
+            telemetry = { enable = false },
             workspace = {
                 maxPreload = 100000,
                 preloadFileSize = 10000,
-                checkThirParty = false,
+                checkThirdParty = false,
                 library = {
+                    [vim.env.VIMRUNTIME] = true,
                     [vim.fn.expand "$VIMRUNTIME/lua"] = true,
                     [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
                     [vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types"] = true,
                     [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
                     ["${3rd}/luv/library"] = true,
+                    ["${3rd}/luassert/library"] = true,
                     [vim.fn.stdpath "data" .. "/site/pack/packer/start/nvim-lspconfig/lua"] = true,
                 },
             },
@@ -33,6 +36,7 @@ return {
                 vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types",
                 vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy",
                 "${3rd}/luv/library",
+                "${3rd}/luassert/library",
                 unpack(vim.api.nvim_get_runtime_file("", true)),
             },
             diagnostic = {
